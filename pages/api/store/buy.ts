f802ffee791204fe965d7d0a7c21ba0c1d6275af
Ryan Albrecht <ryan.albrecht@sentry.io>
2023-10-24 16:27:48 -0700

    Mewtwo cannot be purchased

--- pages/api/store/buy.ts
+++ pages/api/store/buy.ts
@@ -1,9 +1,16 @@
-import type { NextApiRequest } from "next";
+import type { NextApiRequest, NextApiResponse } from "next";
 
-export default async function ApiPokemonFeatured(req: NextApiRequest) {  
-  await new Promise(resolve => {
-    setTimeout(resolve, 3000);
-  });
+export default async function ApiPokemonFeatured(req: NextApiRequest, res: NextApiResponse) {  
+  const items: Array<[string, number]> = JSON.parse(req.body);
 
-  throw new Error('Duplicate key violates unique constraint')
+  const hasMewtwo = items.some(([name, amount]) => name === 'mewtwo');
+  if (hasMewtwo) {
+    await new Promise(resolve => {
+      setTimeout(resolve, 4000);
+    });
+
+    res.status(500).json({error: "Invalid Pokemon selected"});
+  } else {
+    res.status(200).json({success: true});
+  }
 }

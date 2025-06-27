4a0f3de4011077805405707370a65ea4649cb3f7
Ryan Albrecht <ryan.albrecht@sentry.io>
2025-06-27 09:24:26 -0700

    chore: Expand the stub/mock feature flag overrides list

--- src/components/featureFlags/stubFeatureFlagAdapter.ts
+++ src/components/featureFlags/stubFeatureFlagAdapter.ts
@@ -5,7 +5,10 @@ let isCleared = false;
 const stubFeatureFlagAdapter: FeatureFlagAdapter = {
   getFlagMap: () => ({}),
   getOverrides: () => (isCleared ? {} : {
-    foo: true
+    foo: true,
+    bar: true,
+    biz: true,
+    baz: true,
   }),
   setOverride: () => {},
   clearOverrides: () => { isCleared = true},
